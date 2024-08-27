c80fed1f48f2367b8f564b92fac5a977434bd468
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-08-27 12:16:34 -0400

    feat(major): Now show full component class and can copy them

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
   const [hover, setHover] = useState(false);
   const [code, setCode] = useState(false);
 
-
   useEffect(() => {
     //recover temp data
     const storedColor = localStorage.getItem('color');
@@ -138,45 +137,56 @@ function App() {
       target: { tabId: tab.id! },
       args: [],
       func: () => {
-        let tooltip = document.createElement('div');
+        let tooltip = document.createElement('tooltip');
+        //tooltip styles
         tooltip.style.position = 'fixed';
-        tooltip.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-        tooltip.style.color = 'white';
+        tooltip.style.backgroundColor = 'rgba(30, 30, 30)';
         tooltip.style.padding = '5px 10px';
-        tooltip.style.borderRadius = '4px';
+        tooltip.style.border = '1px solid rgba(207, 207, 234)';
         tooltip.style.fontSize = '12px';
+        tooltip.style.color = 'rgba(207, 207, 234)'
         tooltip.style.zIndex = '10000';
         tooltip.style.pointerEvents = 'none';
         tooltip.style.display = 'none';
+
         document.body.appendChild(tooltip);
-
         document.addEventListener('mouseover', (event) => {
-
           const targetElement = event.target as HTMLElement;
-          const classes = targetElement.className;
+          //shows last 2 component classes to edit style
+          const getElementHierarchyWithClasses = (element: HTMLElement): string => {
+            let hierarchy = `${element.tagName.toLowerCase()}${element.className ? '.' + element.className.split(' ').join('.') : ''}`;
+            let count = 1;
 
+            while (element.parentElement && element.parentElement.tagName.toLowerCase() !== 'body' && count < 2) {
+              element = element.parentElement;
+              hierarchy = `${element.tagName.toLowerCase()}${element.className ? '.' + element.className.split(' ').join('.') : ''}>${hierarchy}`;
+              count++;
+            }
+            return hierarchy;
+          };
+
+          const hierarchy = getElementHierarchyWithClasses(targetElement);
           document.addEventListener('mousemove', (event) => {
             tooltip.style.top = `${event.clientY + 10}px`;
             tooltip.style.left = `${event.clientX + 10}px`;
           });
-
           document.addEventListener('mouseout', () => {
             tooltip.style.display = 'none';
           });
-
-
-          if (classes) {
+          if (hierarchy) {
             tooltip.style.display = 'block';
-            tooltip.textContent = `Clases: ${classes}`;
+            tooltip.textContent = `Clase: ${hierarchy}`;
+            document.addEventListener('click', function () {
+              navigator.clipboard.writeText(`${hierarchy}`);
+              tooltip.textContent = `Clase copiada`;
+            })
           } else {
             tooltip.style.display = 'none';
           }
+        });
 
-        })
       }
     });
-
-
   };
 
   showHoverClasses();
@@ -269,11 +279,6 @@ function App() {
 }
 
 // TODO: Pasar codigo estilo de hover a archivo css
-// TODO: Crear funcion para copiar nombre de la clase
-// TODO: Crear funcion para mostrar colores de root
-// TODO: Crear funcion para mostrar posibles clases,
-//       es decir, que especifique mejor que clase
-//       se esta seleccionando
 // TODO: Refactorizar
 
 export default App;
